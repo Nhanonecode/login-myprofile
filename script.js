de8eaac949e827bff5ce953f6181a9fe78da92bc
nhanonecode <nguyenminhnhan2310@gmail.com>
2025-04-01 22:30:12 +0700

    p2 login

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ registerLink.onclick = () => {
 loginLink.onclick = () => {
   wrapper.classList.remove("active");
 };
+
+//function login
+
+const listAccount = [{ username: "admin", password: "123" }];
+let isLogin = !!localStorage.getItem("token");
+
+function CheckLogin() {
+  if (isLogin) {
+    window.location.href = "profile.html";
+  }
+}
+
+function Login() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let checkLogin = listAccount.some(
+    (value) => value.username === username && value.password === password
+  );
+  console.log(checkLogin);
+  if (checkLogin) {
+    localStorage.setItem("token", username);
+    isLogin = true;
+    CheckLogin();
+  } else {
+    alert("wrong username or password !");
+  }
+}
